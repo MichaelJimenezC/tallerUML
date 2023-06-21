229e7942c5fc353bc0d8e1823b70f3cf15964a17
MichaelJimenezC <micjejim@espol.edu.ec>
2023-06-21 16:02:25 -0500

    subir

---
+++ tallerUML/src/main/java/com/mycompany/talleruml/SoporteTecnico.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.talleruml;
+
+/**
+ *
+ * @author CltControl
+ */
+public class SoporteTecnico extends Usuario{
+    
+    public SoporteTecnico(String usuario, String contraseña, String nombre, String apellido) {
+        super(usuario, contraseña, nombre, apellido);
+    }
+    
+    public void brindarAsistencia(Usuario usuario){
+        
+    }
+    public void resolverProblemaTecnico(Usuario usuario){
+        
+    }
+    public void derivarProblemaDeGestion(Administrador administrador){
+        
+    }
+    
+}
